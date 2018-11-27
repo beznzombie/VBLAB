6023a863c57348ee2d967fb91606f3971ebb5efc
Chaloemrat <bezn_zombie2@hotmail.com>
2018-11-27 15:08:52 +0700

    VB Lab 1

---
+++ Myproject/Myproject/FomMyFirst.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtSalary = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblSalaryPYear = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(176, 257)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtSalary
+        '
+        Me.txtSalary.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.txtSalary.Location = New System.Drawing.Point(192, 112)
+        Me.txtSalary.Name = "txtSalary"
+        Me.txtSalary.Size = New System.Drawing.Size(146, 20)
+        Me.txtSalary.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.DarkOrange
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.Location = New System.Drawing.Point(86, 109)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 23)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "รับเงิน"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.BurlyWood
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label3.Location = New System.Drawing.Point(111, 222)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 23)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "เงินหลังหักภาษี"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblTotal.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.lblTotal.Location = New System.Drawing.Point(217, 222)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotal.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Salmon
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label2.Location = New System.Drawing.Point(111, 190)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 23)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "ภาษี"
+        '
+        'lblTax
+        '
+        Me.lblTax.BackColor = System.Drawing.Color.Bisque
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTax.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblTax.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.lblTax.Location = New System.Drawing.Point(217, 190)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(100, 23)
+        Me.lblTax.TabIndex = 2
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.PeachPuff
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label5.Location = New System.Drawing.Point(111, 156)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 23)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "เงินเดือนต่อปี"
+        '
+        'lblSalaryPYear
+        '
+        Me.lblSalaryPYear.BackColor = System.Drawing.Color.Linen
+        Me.lblSalaryPYear.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSalaryPYear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblSalaryPYear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.lblSalaryPYear.Location = New System.Drawing.Point(217, 156)
+        Me.lblSalaryPYear.Name = "lblSalaryPYear"
+        Me.lblSalaryPYear.Size = New System.Drawing.Size(100, 23)
+        Me.lblSalaryPYear.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label4.Location = New System.Drawing.Point(267, 309)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(171, 48)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "นายเฉลิมรัฐ  แบบอย่าง" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "60342110001-8"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 366)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblSalaryPYear)
+        Me.Controls.Add(Me.lblTax)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSalary)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtSalary As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblTax As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents lblSalaryPYear As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Myproject/Myproject/FomMyFirst.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        lblSalaryPYear.Text = Val(txtSalary.Text) * 12
+        lblTax.Text = Val(lblSalaryPYear.Text) * 0.05
+        lblTotal.Text = Val(lblSalaryPYear.Text) - Val(lblTax.Text)
+    End Sub
+End Class
